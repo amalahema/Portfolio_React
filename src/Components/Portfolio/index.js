61c1a4b8846dd10bad52009f09e8238a5e769b17
Amala Hema <amalahema@gmail.com>
2023-06-22 15:44:49 -0400

    proiect3 div moved to first

--- src/Components/Portfolio/index.js
+++ src/Components/Portfolio/index.js
@@ -2,81 +2,80 @@ import React from "react";
 import workout from "../../Assets/workout.png";
 import sales from "../../Assets/sales.jpg";
 import event from "../../Assets/event.PNG";
-function Portfolio()
-{
-    return(
-        
-          <div>
+function Portfolio() {
+  return (
 
-          <div className="project-info">
-            <div className="project-img">
-              <a href="https://github.com/amalahema/go-super-team-1-">
-                {" "}
-                <img
-                  src={workout}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="Workout_Dashboard"
-                />
-              </a>
-            </div>
-            <div className="project-summary">
-              <h4>Workout Dashboard</h4>
-              <p>
-               Workout and Nutritional API are used in this project.This project filters the data as the visual format from the API
-              </p>
-            </div>
-          </div>
-          <div className="project-info">
+    <div>
+      <div className="project-info">
 
-            <div className="project-img">
-              <a href="https://github.com/amalahema/Jungslist">
-                {" "}
-                <img
-                  src={sales}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="Online Marketplace"
-                />
-              </a>
-            </div>
+        <div className="project-img">
+          <a href="https://flawlessfunction.herokuapp.com/">
+            {" "}
+            <img
+              src={event}
+              className="my-2"
+              style={{ width: "100%" }}
+              alt="Event Planner"
+            />
+          </a>
+        </div>
 
-            <div className="project-summary">
-              <h4>Marketplace</h4>
-              <p>
-              This site offers many different products from many different sellers
-              </p>
-            </div>
+        <div className="project-summary">
+          <h4>Event PLanner</h4>
+          <p>
+            We can add, update & delete event using Event Planner
+          </p>
+        </div>
 
-          </div>
+      </div>
+
+      <div className="project-info">
+        <div className="project-img">
+          <a href="https://github.com/amalahema/go-super-team-1-">
+            {" "}
+            <img
+              src={workout}
+              className="my-2"
+              style={{ width: "100%" }}
+              alt="Workout_Dashboard"
+            />
+          </a>
+        </div>
+        <div className="project-summary">
+          <h4>Workout Dashboard</h4>
+          <p>
+            Workout and Nutritional API are used in this project.This project filters the data as the visual format from the API
+          </p>
+        </div>
+      </div>
+      <div className="project-info">
+
+        <div className="project-img">
+          <a href="https://github.com/amalahema/Jungslist">
+            {" "}
+            <img
+              src={sales}
+              className="my-2"
+              style={{ width: "100%" }}
+              alt="Online Marketplace"
+            />
+          </a>
+        </div>
+
+        <div className="project-summary">
+          <h4>Marketplace</h4>
+          <p>
+            This site offers many different products from many different sellers
+          </p>
+        </div>
+
+      </div>
 
 
-          <div className="project-info">
 
-            <div className="project-img">
-              <a href= "https://flawlessfunction.herokuapp.com/">
-                {" "}
-                <img
-                  src={event}
-                  className="my-2"
-                  style={{ width: "100%" }}
-                  alt="Event Planner"
-                />
-              </a>
-            </div>
+    </div>
 
-            <div className="project-summary">
-              <h4>Event PLanner</h4>
-              <p>
-                We can add, update & delete event using Event Planner
-              </p>
-            </div>
 
-          </div>
-
-        </div> 
-        
-      
-    );
+  );
 }
 export default Portfolio;
